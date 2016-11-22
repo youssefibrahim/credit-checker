a246dffe9c140b2979e83bb5f812f278bbe01bc8
Youssef Ibrahim <yibrahim@blackberry.com>
2016-11-22 15:47:01 -0500

    able to group courses, and check non-course requirements

--- credit_checker.py
+++ credit_checker.py
@@ -1,16 +1,11 @@
 # http://stackoverflow.com/questions/7460938/how-to-run-python-script-in-webpage
 
-ECE_1A = [100, 105, 140, 150]
+from credits import *
 
-COURSES_1A = ['CHE 102', 'MATH 117']
-
-ECE_1B = ['ECE 100', 'ECE 103', 'ECE 106', 'ECE 124', 'ECE 155']
 
 lines = [line.rstrip('\n') for line in open('transcript.txt')]
 lines = filter(None, lines)
-# print lines
-SEPERATOR = '__________________________________________________________________________________________'
-COURSES_START = 'Course      Description                Attempt/Earn  Grade  Credit InGPA  Desig   DesGrd  '
+
 
 def get_terms(transcript):
 	indices = find_indices(transcript, SEPERATOR)
@@ -35,12 +30,83 @@ def get_courses_per_term(term):
 		courses.append(course)
 	return courses
 
+def get_courses(terms):
+	courses = []
+	for term in terms:
+		courses.extend(get_courses_per_term(term))
+	return courses
+
+def extract_course_name(course):
+	return ' '.join(course[:2])
+
+def join_course_names(courses):
+	list_of_courses = []
+	for course in courses:
+		list_of_courses.append(' '.join(course))
+	return list_of_courses
+
+def group_courses(courses):
+	PD = []
+	ECE = []
+	CSE = []
+	NSE = []
+	WKRPT = []
+	COOP = []
+	for course in courses:
+		if course.startswith('PD'):
+			PD.append(course)
+
+		elif course.startswith('ECE'):
+			ECE.append(course)
+
+		elif course.startswith('WKRPT'):
+			WKRPT.append(course)
+
+		elif course.startswith('COOP'):
+			COOP.append(course)
+
+		elif course in nse_courses_list_1 or course in nse_courses_list_2:
+			NSE.append(course)
+
+		else:
+			CSE.append(course)
+	return PD, ECE, CSE, NSE, WKRPT, COOP
 
 
-terms = get_terms(lines)
-courses = []
-for term in terms:
-	courses.extend(get_courses_per_term(term))
-courses = filter(None, courses)
-import pdb
-pdb.set_trace()
+def check_requirements(courses):
+	PD, ECE, CSE, NSE, WKRPT, COOP = group_courses(courses)
+	
+	check_non_course(PD, 5)
+	check_non_course(WKRPT, 3)
+	check_non_course(COOP, 5)
+
+	check_courses(ECE)
+	check_courses(CSE)
+	check_courses(NSE)
+
+
+def check_non_course(satisfied, requirement):
+	name = satisfied[0].split()[0]
+	if len(satisfied)<requirement:
+		print("WARNING: You currently have {} {}, but require at least {}".format(len(satisfied), name, requirement))
+	else:
+		print("You've met requirements for {}".format(name))
+
+def check_courses(courses):
+	
+
+
+
+if __name__ == "__main__":
+	terms = get_terms(lines)
+	courses = get_courses(terms)
+	courses = [course for course in courses if len(course) >= 5]
+	courses = [extract_course_name(course) for course in courses]
+ 	for item in remove:
+		courses = filter(lambda course: not course.startswith(item[0]), courses)
+	# courses = join_course_names(courses)
+	
+	check_requirements(courses)
+	
+	import pdb
+	pdb.set_trace()
---
+++ credits.py
@@ -0,0 +1,16 @@
+remove = ([['Term', 'Avg'], ['Cumulative', 'Avg'], ['Total', 'Units'],
+		  ['Term', 'summary'],['Academic', 'Standing:'],['Notes:', 'ECE'], ['E&P', 'Decision']])
+
+nse_courses_list_1 = ['BIOL 130', 'BIOL 130L', 'BIOL 240', 'BIOL 240L', 'BIOL 273', 'BIOL 273L',
+			   'CHEM 123', 'CHEM 123L', 'CHEM 237' , 'CHEM 237L', 'CHEM 262',  'CHEM 262L', 'ECE 403',
+			   'ECE 404', 'ECE 405', 'ENVS 200', 'NE 122']
+
+nse_courses_list_2 = ['BIOL 110', 'BIOL 130', 'BIOL 150', 'BIOL 165', 'BIOL 211', 'BIOL 240',
+					  'BIOL 241', 'BIOL 273', 'CHE 161', 'CHEM 123', 'CHEM 209', 'CHEM 217', 
+					  'CHEM 237', 'CHEM 254', 'CHEM 262', 'CHEM 266', 'CHEM 356', 'CHEM 404', 
+					  'EARTH 121', 'EARTH 122', 'EARTH 123', 'EARTH 221', 'EARTH 270', 'EARTH 281', 
+					  'ECE 209', 'ENVE 275', 'ENVE 276', 'PHYS 234', 'PHYS 263', 'PHYS 75', 'PHYS 280', 
+					  'PHYS 334', 'PHYS 335', 'PHYS 375', 'PHYS 380', 'SCI 238']
+
+SEPERATOR = '__________________________________________________________________________________________'
+COURSES_START = 'Course      Description                Attempt/Earn  Grade  Credit InGPA  Desig   DesGrd  '
