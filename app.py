a0b75e49d5231a67fdcc7cb85a6f47d07d1229e7
yibrahim <ymibrahi@uwaterloo.ca>
2016-12-14 11:11:57 -0500

    changing ports again, trying to force it on port 8000 on heroku

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def get_missing_number(courses, key):
 	return courses[key]['num_missing']
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
